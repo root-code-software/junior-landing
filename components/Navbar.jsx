b777059d82ce9c028f7b59de130589d3d763ef01
Jose Yepez <josyepe97@gmail.com>
2023-01-12 13:53:07 -0500

    main-front

---
+++ components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Navbar.module.css";
+import logo from "../public/logo.png";
+
+const Navbar = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.containerNavbar}>
+        <Image src={logo} alt="/" width="125" height="50" />
+        <div>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className={styles.item}>Inicio</li>
+            </Link>
+            <Link href="/">
+              <li className={styles.item}>Proyectos</li>
+            </Link>
+            <Link href="/">
+              <li className={styles.item}>Planes</li>
+            </Link>
+            <Link href="/">
+              <li className={styles.item}>Sobre Nosotros</li>
+            </Link>
+          </ul>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ pages/_app.js
@@ -0,0 +1,16 @@
+import Navbar from '../components/Navbar';
+import Main from '../components/Main'
+
+import '../styles/globals.css';
+
+function MyApp({ }) {
+  return (
+    <>
+      <Navbar />
+      <Main />
+
+    </>
+  );
+}
+
+export default MyApp;
