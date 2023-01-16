5905a4f3b39b6f823eb6d25948c0cd0509d29da8
Jose Yepez <josyepe97@gmail.com>
2023-01-16 16:42:04 -0500

    Responsive-design

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => (
           <Link href="/">
             <li className={styles.item}>Inicio</li>
           </Link>
-          <Link href='/#projects'>
+          <Link href='/#Projects'>
             <li className={styles.item}>Proyectos</li>
           </Link>
           <Link href="/">
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -4,40 +4,52 @@ import logo from "../public/logo.png";
 import styles from "../styles/Projects.module.css";
 
 const Projects = () => (
-    <div id="projects" className={styles.container}>
-      <div className={styles.containerLogo}>
-        <div className="col-span-4">
-          <h1>Algunos de nuestros proyectos</h1>
-     </div>
-        <div className={styles.logoprojects}>
-          <Image src={logo} className="rounded-xl" alt="/" width="250" height="400"/><br/>
-          <h4>
-            Lorem ipsum dolor sit amet<br/>
-            consectetur adipisicing elit.<br/>
-            Nihil molestias nobis sequi
-          </h4>
-     </div>
-        <div className={styles.logoprojects}>
-          <Image src={logo} className="rounded-xl" alt="/" width="250" height="400"/><br/>
-          <h4 className="text-center">
-            Lorem ipsum dolor sit amet<br/>
-            consectetur adipisicing elit.<br/>
-            Nihil molestias nobis sequi
-          </h4>
-        </div>
-        <div className={styles.logoprojects}>
-          <Image src={logo}className="rounded-xl" alt="/" width="250" height="400"/><br/>
-          <h4 className="text-center">
-            Lorem ipsum dolor sit amet<br/>
-            consectetur adipisicing elit. <br/>
-            Nihil molestias nobis sequi
-          </h4>
-        </div>
-        <br />
-        <div className="col-span-4">
-          <h3>Si quieres ver mas proyectos puedes verlos aqui</h3><br/>
-          <button href="/">Ver mas</button>
-        </div>
+    <div id="Projects" className="w-full lg:h-screen p-2">
+      <div className={styles.container}>
+        <h1 className="py-4">Algunos de nuestros proyectos</h1><br/>
+        <div className={styles.containerLogo}>
+          <div>
+            <div className={styles.logoProjects}>
+              <div className="m-auto">
+              <Image src={logo} className="rounded-xl" alt="/" width="250" height="400"/> 
+              </div>
+              <div className={styles.descriptionProjects}>
+                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                   Inventore, nobis. Facilis ad officia deserunt expedita nostrum 
+                   similique porro consequatur ipsam facere quis. 
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div>
+          <div className={styles.logoProjects}>
+              <div className="m-auto">
+              <Image src={logo} className="rounded-xl" alt="/" width="250" height="400"/> 
+              </div>
+              <div className={styles.descriptionProjects}>
+                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                   Inventore, nobis. Facilis ad officia deserunt expedita nostrum 
+                   similique porro consequatur ipsam facere quis. 
+                </h4>
+              </div>
+            </div>
+          </div>
+          <div>
+           <div className={styles.logoProjects}>
+              <div className="m-auto">
+              <Image src={logo} className="rounded-xl" alt="/" width="250" height="400"/> 
+              </div>
+              <div className={styles.descriptionProjects}>
+                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                   Inventore, nobis. Facilis ad officia deserunt expedita nostrum 
+                   similique porro consequatur ipsam facere quis. 
+                </h4>
+              </div>
+            </div>
+          </div>
+        </div><br/>
+        <h3 className="py-4">Si quieres ver mas proyectos puedes verlos aqui</h3>
+        <button className={styles.btn}>Ver mas</button>
       </div>
     </div>
   );
