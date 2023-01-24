f4a8511a9ec801ce76f3d9459a92d0f7db50c9bf
Jose Yepez <josyepe97@gmail.com>
2023-01-24 17:29:25 -0500

    show-projects

--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,30 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import logo from "../public/logo.png";
 import styles from "../styles/Projects.module.css";
 
-const Projects = () => (
-    <div id="Projects" className="w-full lg:h-screen p-2">
+const Projects = () => {
+ const [show, setShow] = useState(true);
+
+  return (
+    <div id="projects" className="w-full lg:h-screen p-2">
       <div className={styles.container}>
         <h1 className="py-4">Algunos de nuestros proyectos</h1><br/>
         <div className={styles.containerLogo}>
-          <div>
             <div className={styles.logoProjects}>
               <div className="m-auto">
-              <Image src={logo} className="rounded-xl" alt="/" width="250" height="400"/> 
+              <Image src={logo} className="rounded-xl" alt="Imagen del Proyecto #1" width="250" height="400"/> 
               </div>
               <div className={styles.descriptionProjects}>
                 <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                    Inventore, nobis. Facilis ad officia deserunt expedita nostrum 
                    similique porro consequatur ipsam facere quis. 
                 </h4>
-              </div>
             </div>
           </div>
           <div>
           <div className={styles.logoProjects}>
               <div className="m-auto">
-              <Image src={logo} className="rounded-xl" alt="/" width="250" height="400"/> 
+              <Image src={logo} className="rounded-xl" alt="Imagen del Proyecto #2" width="250" height="400"/> 
               </div>
               <div className={styles.descriptionProjects}>
                 <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
@@ -37,7 +38,7 @@ const Projects = () => (
           <div>
            <div className={styles.logoProjects}>
               <div className="m-auto">
-              <Image src={logo} className="rounded-xl" alt="/" width="250" height="400"/> 
+              <Image src={logo} className="rounded-xl" alt="Imagen del Proyecto #3" width="250" height="400"/> 
               </div>
               <div className={styles.descriptionProjects}>
                 <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
@@ -47,11 +48,53 @@ const Projects = () => (
               </div>
             </div>
           </div>
+
+          { show &&
+          <div className={styles.logoProjects}>
+              <div className="m-auto">
+              <Image src={logo} className="rounded-xl" alt="Imagen del Proyecto #4" width="250" height="400"/> 
+              </div>
+              <div className={styles.descriptionProjects}>
+                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                   Inventore, nobis. Facilis ad officia deserunt expedita nostrum 
+                   similique porro consequatur ipsam facere quis. 
+                </h4>
+              </div>
+            </div>
+               }{show &&
+          <div>
+          <div className={styles.logoProjects}>
+              <div className="m-auto">
+              <Image src={logo} className="rounded-xl" alt="Imagen del Proyecto #5" width="250" height="400"/> 
+              </div>
+              <div className={styles.descriptionProjects}>
+                <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                   Inventore, nobis. Facilis ad officia deserunt expedita nostrum 
+                   similique porro consequatur ipsam facere quis. 
+                </h4>
+              </div>
+            </div>
+          </div>
+          } {show &&
+            <div>
+            <div className={styles.logoProjects}>
+                <div className="m-auto">
+                <Image src={logo} className="rounded-xl" alt="Imagen del Proyecto #6" width="250" height="400"/> 
+                </div>
+                <div className={styles.descriptionProjects}>
+                  <h4>Lorem ipsum dolor, sit amet consectetur adipisicing elit.
+                     Inventore, nobis. Facilis ad officia deserunt expedita nostrum 
+                     similique porro consequatur ipsam facere quis. 
+                  </h4>
+                </div>
+              </div>
+            </div>
+            }
         </div><br/>
         <h3 className="py-4">Si quieres ver mas proyectos puedes verlos aqui</h3>
-        <button className={styles.btn}>Ver mas</button>
+        <button className={styles.btn} onClick={() => setShow(!show)}>{show === true ? "Ver menos"  : "Ver mas"}</button><br/>
       </div>
     </div>
   );
-
+};
 export default Projects;
