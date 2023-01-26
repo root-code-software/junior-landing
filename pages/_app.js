2d71029a77c311eb1db3dd500408c3eba06d1273
Jose Yepez <josyepe97@gmail.com>
2023-01-26 17:36:26 -0500

    Revert "cambios"
    This reverts commit 7a6d44377e964a154b3ff27988a9adbb54ccf795.

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Navbar from "../components/Navbar";
 import Main from "../components/Main";
 import Achievements from "../components/Achievements";
+
 import "../styles/globals.css";
 
 const MyApp = () => (
