7a6d44377e964a154b3ff27988a9adbb54ccf795
Jose Yepez <josyepe97@gmail.com>
2023-01-26 17:20:26 -0500

    cambios

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import Navbar from "../components/Navbar";
 import Main from "../components/Main";
 import Achievements from "../components/Achievements";
-
 import "../styles/globals.css";
 
 const MyApp = () => (
